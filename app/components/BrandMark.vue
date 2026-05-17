baae46b93a5ac53c49c1c14227d7fc5c0ce298d2
rubentouitou <rubencharlestouitou@gmail.com>
2026-05-17 12:39:10 -0400

    Header uses /img/logo-wide.png (468x29 banner); footer + admin keep tall logo.png

--- app/components/BrandMark.vue
+++ app/components/BrandMark.vue
@@ -2,18 +2,29 @@
 defineProps<{
   variant?: 'light' | 'dark'
   height?: number
+  wide?: boolean
 }>()
 </script>
 
 <template>
   <NuxtLink to="/" class="inline-flex items-center no-underline" aria-label="Aldridge and Charles Marine">
     <img
+      v-if="wide"
+      src="/img/logo-wide.png"
+      alt="Aldridge & Charles Marine"
+      width="468"
+      height="29"
+      :style="{ height: (height || 22) + 'px', width: 'auto' }"
+      :class="variant === 'light' ? 'brightness-0 invert' : ''"
+    />
+    <img
+      v-else
       src="/img/logo.png"
       alt="Aldridge & Charles Marine"
-      :style="{ height: (height || 30) + 'px', width: 'auto' }"
-      :class="variant === 'light' ? 'brightness-0 invert' : ''"
       width="469"
       height="144"
+      :style="{ height: (height || 30) + 'px', width: 'auto' }"
+      :class="variant === 'light' ? 'brightness-0 invert' : ''"
     />
   </NuxtLink>
 </template>
